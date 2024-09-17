1d14e5c4bc48b90ade0e72043fec659e6bea1c49
sidharth tripathi <sidharthtripathi807@gmail.com>
2024-09-17 13:12:06 +0530

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -31,17 +31,12 @@ export default async function Component() {
     }
   });
   if (!user) return redirect('/join');
-  let postCount = 0; // this may need to memoize
-  user.followings.filter((user) => {
-    postCount += user.posts.length;
-    return false;
-  });
   return (
     <div className="flex h-full flex-col">
       <main className="flex-1">
         <div className="container mx-auto grid grid-cols-1 gap-8 px-4 py-8 md:grid-cols-3 md:px-6">
           <div className="col-span-2 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {postCount > 0 ? (
+            {
               user.followings
                 .map((user) => {
                   return user.posts.map((post) => {
@@ -58,14 +53,7 @@ export default async function Component() {
                   });
                 })
                 .flat()
-            ) : (
-              <div className="col-start-1 col-end-[-1] text-center">
-                <p className="text-xl font-bold">Your Feed is Empty</p>
-                <p className="text-sm text-muted-foreground">
-                  People You follow will appear here
-                </p>
-              </div>
-            )}
+              }
           </div>
           <SuggestionsCard />
         </div>
