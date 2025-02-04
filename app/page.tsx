07e4ca769e54b361fa3e6e409ad2057ee2bb4411
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-02-04 19:35:35 +0530

    add empty feed page view

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
 import { redirect } from 'next/navigation';
-import {PostCard} from '@/components/PostCard';
+import { PostCard } from '@/components/PostCard';
 import { prisma } from '@/lib/prisma';
 import { headers } from 'next/headers';
 import SuggestionsCard from '@/components/SuggestionsCard';
+import Link from 'next/link';
+import { CameraIcon, PodcastIcon, TvIcon } from 'lucide-react';
 
 // no pagination as of now
 export default async function Homepage() {
@@ -20,25 +21,25 @@ export default async function Homepage() {
           avatar: true,
           username: true,
           posts: {
-            orderBy : {createdAt : "asc"},
+            orderBy: { createdAt: 'asc' },
 
             select: {
-              bookmarkedBy : {
-                where : {
+              bookmarkedBy: {
+                where: {
                   username
                 },
-                take : 1,
-                select : {id:true}
+                take: 1,
+                select: { id: true }
               },
               postURL: true,
               caption: true,
               id: true,
-              likedBy : {
-                where : {
-                  username,
+              likedBy: {
+                where: {
+                  username
                 },
-                take : 1,
-                select : {id : true}
+                take: 1,
+                select: { id: true }
               }
             }
           }
@@ -47,28 +48,41 @@ export default async function Homepage() {
     }
   });
   if (!user) return redirect('/join');
+  let postCount = 0;
+  user.followings.forEach((user) => {
+    postCount += user.posts.length;
+  });
   return (
-    <main className='flex container mt-4 gap-4'>
-      <section className='flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {user.followings
-          .map((user) => {
-            return user.posts.map((post) => {
-              return (
-                <PostCard
-                  key={post.id}
-                  avatar={user.avatar!}
-                  caption={post.caption!}
-                  postId={post.id}
-                  postURL={post.postURL}
-                  username={user.username}
-                  liked = {post.likedBy.length > 0 ? true : false}
-                  bookmarked = {post.bookmarkedBy.length > 0 ? true : false}
-                />
-              );
-            });
-          })
-          .flat()}
-      </section>
+    <main className="container mt-4 flex gap-4">
+      {postCount == 0 ? (
+        <div className='flex-grow h-full space-y-4 mt-4'>
+          <TvIcon size={60} className='mx-auto'/>
+          <p className='text-3xl text-center font-bold'>No Posts yet.</p>
+          <p className=' text-center'>Looks like no one posted any content yet. <br/>Be the first one <Link href="/create" className='underline'>Here</Link></p>
+        </div>
+      ) : (
+        <section className="grid flex-grow grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {user.followings
+            .map((user) => {
+              return user.posts.map((post) => {
+                return (
+                  <PostCard
+                    key={post.id}
+                    avatar={user.avatar!}
+                    caption={post.caption!}
+                    postId={post.id}
+                    postURL={post.postURL}
+                    username={user.username}
+                    liked={post.likedBy.length > 0 ? true : false}
+                    bookmarked={post.bookmarkedBy.length > 0 ? true : false}
+                  />
+                );
+              });
+            })
+            .flat()}
+        </section>
+      )}
+
       <section>
         <SuggestionsCard />
       </section>
