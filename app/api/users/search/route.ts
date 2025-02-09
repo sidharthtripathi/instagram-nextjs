5bf354442e17f87b9bcbffa29c9b96fdbe8a7d9a
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-02-10 01:26:10 +0530

    add full text search for user searching

--- app/api/users/search/route.ts
+++ app/api/users/search/route.ts
@@ -3,24 +3,12 @@ import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  // @ts-ignore
   const searchParams = new URLSearchParams(new URL(req.url).searchParams);
   const userQuery = searchParams.get('q') as string;
   if (!userQuery) return NextResponse.json([]);
   const userList = await prisma.user.findMany({
     where: {
-      OR: [
-        {
-          username: {
-            contains: userQuery
-          }
-        },
-        {
-          name: {
-            contains: userQuery
-          }
-        }
-      ]
+      OR : [{username : {search : userQuery}},{name : {search : userQuery}}]
     },
     select: {
       username: true,
