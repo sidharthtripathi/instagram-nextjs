82ebc86de0c5dbbb92e0510554a8864cd6cc8881
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-02-09 15:31:11 +0530

    removed dropdown menu for unauthenticated users

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,11 +8,14 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Button } from './ui/button';
 import { ModeToggle } from './ui/darkModeToggle';
-import ProfileNavigator from './ProfileNavigator';
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { BadgePlus, Menu, Search } from 'lucide-react';
+import { headers } from 'next/headers';
+import Logout from './LougoutButton';
+import { unstable_noStore as noStore } from 'next/cache';
 
 function Navbar() {
+  const username = headers().get('username');
+  
   return (
     <header className="flex h-14 items-center px-4 lg:px-6">
       <Link
@@ -29,7 +32,7 @@ function Navbar() {
           className="rounded-full p-1 hover:bg-accent"
           prefetch={false}
         >
-          <BadgePlus className='size-4' />
+          <BadgePlus className="size-4" />
         </Link>
         <Link
           href="/explore"
@@ -40,46 +43,56 @@ function Navbar() {
         </Link>
 
         <ModeToggle />
-
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="rounded-full">
-              <Menu className="size-4" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem asChild>
-              <Link href="/" className="w-full" prefetch={false}>
-                Home
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/explore" className="w-full" prefetch={false}>
-                Explore
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/saved" className="w-full" prefetch={false}>
-                Saved
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/liked" className="w-full" prefetch={false}>
-                Liked
-              </Link>
-            </DropdownMenuItem>
-            <ProfileNavigator />
-          </DropdownMenuContent>
-        </DropdownMenu>
+        {username ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" className="rounded-full">
+                <Menu className="size-4" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem asChild>
+                <Link href="/" className="w-full" prefetch={false}>
+                  Home
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/explore" className="w-full" prefetch={false}>
+                  Explore
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/saved" className="w-full" prefetch={false}>
+                  Saved
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/liked" className="w-full" prefetch={false}>
+                  Liked
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link
+                  href={`/users/${username}`}
+                  className="w-full"
+                  prefetch={false}
+                >
+                  Profile
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Logout />
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : null}
       </nav>
     </header>
   );
 }
 
 export default Navbar;
-
-
 
 function MountainIcon(props: any) {
   return (
@@ -99,4 +112,3 @@ function MountainIcon(props: any) {
     </svg>
   );
 }
-
